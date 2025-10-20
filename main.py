df2f83f49e0b279d1d10cffd67643db495a256ea
KyroLesya <o.shentsova.var@gmail.com>
2025-10-20 22:22:43 +0300

    енамы

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+from enum import Enum
+
+
+class Gender(Enum):
+    MALE = "Мужской"
+    FEMALE = "Женский"
+
+
+class DogBreed(Enum):
+    LABRADOR = "Лабрадор"
+    HUSKY = "Хаски"
+    SHEPHERD = "Овчарка"
+
+
+class CatBreed(Enum):
+    MAINE_COON = "Мейн-кун"
+    PERSIAN = "Персидская"
+    SPHINX = "Сфинкс"
+
+
 class Owner:
-    def __init__(self, name: str, age: int, gender: str):
+    def __init__(self, name: str, age: int, gender: Gender):
         self.name = name
         self.age = age
         self.gender = gender
 
 
 class Pet:
-    def __init__(self, name: str, age: int, id: int):
+    def __init__(self, name: str, age: int, gender: Gender):
         self.name = name
         self.age = age
-        self.id = id
+        self.gender = gender
 
 
 class PetDocument:
-    def __init__(self, id: int, issue_date: str):
-        self.id = id
+    def __init__(self, issue_date: str):
         self.issue_date = issue_date
 
 
@@ -25,13 +44,13 @@ class Organization:
 
 
 class Dog(Pet):
-    def __init__(self, breed: str):
+    def __init__(self, breed: DogBreed):
         super().__init__()
         self.breed = breed
 
 
 class Cat(Pet):
-    def __init__(self, breed: str):
+    def __init__(self, breed: CatBreed):
         super().__init__()
         self.breed = breed
 
@@ -46,6 +65,7 @@ class MedicalCard(PetDocument):
     def __init__(self, name: str, disease: str):
         super().__init__()
         self.name = name
+        self.disease = disease
 
 
 class PetShop(Organization):

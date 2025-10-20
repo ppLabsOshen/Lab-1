2ac5943b47d92724cede79acd7b5f997e4f6e974
KyroLesya <o.shentsova.var@gmail.com>
2025-10-21 02:02:18 +0300

    Доработанные енамы и начало переделывания основных классов

--- main.py
+++ main.py
@@ -1,28 +1,82 @@
-from enum import Enum
+from enum import Enum # Для использования Enum
+from typing import List # Для указания переменной как список элементов определенного типа.
 
 
 class Gender(Enum):
+    """Пол питомца или владельца"""
     MALE = "Мужской"
     FEMALE = "Женский"
 
 
 class DogBreed(Enum):
+    """Породы собак"""
     LABRADOR = "Лабрадор"
     HUSKY = "Хаски"
     SHEPHERD = "Овчарка"
 
 
 class CatBreed(Enum):
+    """Породы кошек"""
     MAINE_COON = "Мейн-кун"
     PERSIAN = "Персидская"
     SPHINX = "Сфинкс"
 
 
+class Color(Enum):
+    """Цвет питомца"""
+    BLACK = "Черный"
+    WHITE = "Белый"
+    RED = "Рыжый"
+    GRAY = "Серый"
+    BROWN = "Коричневый"
+
+
+class Disease(Enum):
+    """Болезни питомца"""
+    FLU = "Грипп"
+    PARASITES = "Паразиты"
+    ALLERGY = "Аллергия"
+
+
+class Vaccine(Enum):
+    """Вакцины для питомца"""
+    FLU_SHOT = "Прививка от гриппа"
+    PARASITE_SHOT = "Привика от паразитов"
+    GENERAL_SHOT = "Общая вакцина"
+
+
 class Owner:
+    """Владелец питомца"""
     def __init__(self, name: str, age: int, gender: Gender):
         self.name = name
         self.age = age
         self.gender = gender
+        self.pets: List['Pet'] = []
+
+
+class PetDocument:
+    """Родительский класс для документов питомца"""
+    def __init__(self, issue_date: str, pet_name: str, owner: Owner, gender: Gender):
+        self.issue_date = issue_date
+        self.pet_name = pet_name
+        self.owner = owner
+
+
+class Passport(PetDocument):
+    """Паспорт питомца"""
+    def __init__(self, issue_date: str, pet_name: str, owner: Owner, gender: Gender,  pet_type: str, breed: str, color: Color):
+        super().__init__(issue_date, pet_name, owner, gender)
+        self.pet_type = pet_type
+        self.breed = breed
+        self.color = color
+
+
+class MedicalCard(PetDocument):
+    """Медицинская карта питомца"""
+    def __init__(self, issue_date: str, pet_name: str, owner: Owner, gender: Gender):
+        super().__init__(issue_date, pet_name, owner, gender)
+        self.diseases: List[Disease] = []
+        self.vaccines: List[Vaccine] = []
 
 
 class Pet:
@@ -30,11 +84,6 @@ class Pet:
         self.name = name
         self.age = age
         self.gender = gender
-
-
-class PetDocument:
-    def __init__(self, issue_date: str):
-        self.issue_date = issue_date
 
 
 class Organization:
@@ -53,19 +102,6 @@ class Cat(Pet):
     def __init__(self, breed: CatBreed):
         super().__init__()
         self.breed = breed
-
-
-class Passport(PetDocument):
-    def __init__(self, name: str):
-        super().__init__()
-        self.name = name
-
-
-class MedicalCard(PetDocument):
-    def __init__(self, name: str, disease: str):
-        super().__init__()
-        self.name = name
-        self.disease = disease
 
 
 class PetShop(Organization):
